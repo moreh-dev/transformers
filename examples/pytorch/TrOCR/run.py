bb8d6b0ff4591b47e5a9471b462aa00157030cd8
huonglarne <nguyenlanhuong.vietnam@gmail.com>
2023-07-18 18:42:07 +0900

    run trocr

---
+++ examples/pytorch/TrOCR/run.py
@@ -0,0 +1,126 @@
+import pandas as pd
+
+df = pd.read_fwf('/nas/common_data/IAM/gt_test.txt', header=None)
+df.rename(columns={0: "file_name", 1: "text"}, inplace=True)
+del df[2]
+# some file names end with jp instead of jpg, let's fix this
+df['file_name'] = df['file_name'].apply(lambda x: x + 'g' if x.endswith('jp') else x)
+
+
+from sklearn.model_selection import train_test_split
+
+train_df, test_df = train_test_split(df, test_size=0.2)
+# we reset the indices to start from zero
+train_df.reset_index(drop=True, inplace=True)
+test_df.reset_index(drop=True, inplace=True)
+
+
+import torch
+from torch.utils.data import Dataset
+from PIL import Image
+
+class IAMDataset(Dataset):
+    def __init__(self, root_dir, df, processor, max_target_length=128):
+        self.root_dir = root_dir
+        self.df = df
+        self.processor = processor
+        self.max_target_length = max_target_length
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        # get file name + text 
+        file_name = self.df['file_name'][idx]
+        text = self.df['text'][idx]
+        # prepare image (i.e. resize + normalize)
+        image = Image.open(self.root_dir + file_name).convert("RGB")
+        pixel_values = self.processor(image, return_tensors="pt").pixel_values
+        # add labels (input_ids) by encoding the text
+        labels = self.processor.tokenizer(text, 
+                                          padding="max_length", 
+                                          max_length=self.max_target_length).input_ids
+        # important: make sure that PAD tokens are ignored by the loss function
+        labels = [label if label != self.processor.tokenizer.pad_token_id else -100 for label in labels]
+
+        encoding = {"pixel_values": pixel_values.squeeze(), "labels": torch.tensor(labels)}
+        return encoding
+    
+
+from transformers import TrOCRProcessor
+
+processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
+train_dataset = IAMDataset(root_dir='/nas/common_data/IAM//image/',
+                           df=train_df,
+                           processor=processor)
+eval_dataset = IAMDataset(root_dir='/nas/common_data/IAM//image/',
+                           df=test_df,
+                           processor=processor)
+
+
+from transformers import VisionEncoderDecoderModel
+
+model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-stage1")
+
+model.config.decoder_start_token_id = processor.tokenizer.cls_token_id
+model.config.pad_token_id = processor.tokenizer.pad_token_id
+# make sure vocab size is set correctly
+model.config.vocab_size = model.config.decoder.vocab_size
+
+# set beam search parameters
+model.config.eos_token_id = processor.tokenizer.sep_token_id
+model.config.max_length = 64
+model.config.early_stopping = True
+model.config.no_repeat_ngram_size = 3
+model.config.length_penalty = 2.0
+model.config.num_beams = 4
+
+
+from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments
+
+training_args = Seq2SeqTrainingArguments(
+    predict_with_generate=True,
+    evaluation_strategy="steps",
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
+    fp16=True, 
+    output_dir="./",
+    logging_steps=2,
+    save_steps=1000,
+    eval_steps=200,
+    report_to='none',
+)
+
+
+from datasets import load_metric
+
+cer_metric = load_metric("cer")
+
+
+
+def compute_metrics(pred):
+    labels_ids = pred.label_ids
+    pred_ids = pred.predictions
+
+    pred_str = processor.batch_decode(pred_ids, skip_special_tokens=True)
+    labels_ids[labels_ids == -100] = processor.tokenizer.pad_token_id
+    label_str = processor.batch_decode(labels_ids, skip_special_tokens=True)
+
+    cer = cer_metric.compute(predictions=pred_str, references=label_str)
+
+    return {"cer": cer}
+     
+
+from transformers import default_data_collator
+
+# instantiate trainer
+trainer = Seq2SeqTrainer(
+    model=model,
+    tokenizer=processor.feature_extractor,
+    args=training_args,
+    compute_metrics=compute_metrics,
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    data_collator=default_data_collator,
+)
+trainer.train()

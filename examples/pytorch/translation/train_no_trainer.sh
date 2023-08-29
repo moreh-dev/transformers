91c01a16eceaec9ebc59d74969efa18809acdafa
HDThang <thang.huynh@moreh.com.vn>
2023-08-29 16:56:21 +0900

    fix train .sh script

--- examples/pytorch/translation/train_no_trainer.sh
+++ examples/pytorch/translation/train_no_trainer.sh
@@ -23,11 +23,18 @@ mkdir -p "$(dirname $output_dir)"
 ## Using moreh device
 export MOREH_VISIBLE_DEVICE=$device_id
 
+args="
+    --preprocessing_num_workers \
+    --overwrite_output_dir \
+"
+
 accelerate launch run_translation_no_trainer.py \
     --model_name_or_path $model \
     --source_lang en \
     --target_lang ro \
     --dataset_name wmt16 \
     --dataset_config_name ro-en \
+    --per_device_train_batch_size $batch_size \
+    --per_device_eval_batch_size $batch_size \
     --output_dir $output_dir
     2>&1 | tee $log_file

9d1582a16df1f0b4faab19c1f9e0ea491eecc4c7
HDThang <thang.huynh@moreh.com.vn>
2023-09-13 20:50:16 +0900

    fix callback to log metrics each interval

--- examples/pytorch/token-classification/run_ner.py
+++ examples/pytorch/token-classification/run_ner.py
@@ -212,7 +212,6 @@ class DataTrainingArguments:
 class TBTrainerCallback(TrainerCallback):
     "A callback log loss, learning rate, and throughput each logging step"
     start_time = time.time()
-
     def on_step_begin(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
         # count the time after the logging step
         if state.global_step == 0 or state.global_step % args.logging_steps == 1:
@@ -223,12 +222,13 @@ class TBTrainerCallback(TrainerCallback):
             logging_step_runtime = time.time() - self.start_time
             num_samples = args.per_device_train_batch_size * args.logging_steps
             throughput = num_samples / logging_step_runtime
-            state.log_history[-1]["throughput"] = throughput
-            state.log_history[-1]["step"] = state.global_step
             if 'loss' in state.log_history[-1]:
-                print(f'loss: {state.log_history[-1]["loss"]}, lr: {state.log_history[-1]["learning_rate"]}, throughput: {throughput}, step: {state.global_step}')
-            
-        
+                state.log_history[-1]["throughput"] = throughput
+                state.log_history[-1]["step"] = state.global_step
+
+                mlflow.log_metric("lr", state.log_history[-1]["learning_rate"] , step=state.global_step)
+                mlflow.log_metric("throughput", throughput , step=state.global_step)
+                print(f'loss: {state.log_history[-1]["loss"]}, lr: {state.log_history[-1]["learning_rate"]}, throughput: {throughput}, step: {state.global_step}')       
 # Log number of parameters function
 def get_num_parameters(model):
     num_params = 0
@@ -613,11 +613,6 @@ def main():
             data_args.max_train_samples if data_args.max_train_samples is not None else len(train_dataset)
         )
         metrics["train_samples"] = min(max_train_samples, len(train_dataset))
-        for metric_dict in trainer.state.log_history:
-            if 'loss' in metric_dict:
-                mlflow.log_metric('loss', metric_dict['loss'], step=metric_dict['step'])
-                mlflow.log_metric('lr', metric_dict['learning_rate'], step=metric_dict['step'])
-                mlflow.log_metric('throughput', metric_dict['throughput'], step=metric_dict['step'])
         trainer.log_metrics("train", metrics)
         trainer.save_metrics("train", metrics)
         trainer.save_state()

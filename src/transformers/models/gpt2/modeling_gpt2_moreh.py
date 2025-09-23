df6fcce097ad30d5e744e74e04f51ed8c42b73e2
HyungjunOh <hyungjun.oh@moreh.io>
2025-09-23 19:11:58 +0900

    add pipe for gpt

--- src/transformers/models/gpt2/modeling_gpt2_moreh.py
+++ src/transformers/models/gpt2/modeling_gpt2_moreh.py
@@ -1016,6 +1016,13 @@ class GPT2Model(GPT2PreTrainedModel):
         # Initialize weights and apply final processing
         self.post_init()
 
+        # Moreh Config
+        self.moreh_pipeline_layers = []
+        moreh_config = getattr(config, "moreh_config", None)
+        if moreh_config is not None and "pipeline_layers" in moreh_config:
+            self.moreh_pipeline_layers = moreh_config["pipeline_layers"]
+
+
     @add_start_docstrings(PARALLELIZE_DOCSTRING)
     def parallelize(self, device_map=None):
         # Check validity of device_map
@@ -1257,6 +1264,8 @@ class GPT2Model(GPT2PreTrainedModel):
                 for k, v in self.device_map.items():
                     if i == v[-1] and "cuda:" + str(k) != self.last_device:
                         hidden_states = hidden_states.to("cuda:" + str(k + 1))
+            if i in self.moreh_pipeline_layers:
+                hidden_states = torch.moreh.pipeline_assign(hidden_states)
 
         hidden_states = self.ln_f(hidden_states)
 
@@ -1294,6 +1303,12 @@ class GPT2LMHeadModelMoreh(GPT2PreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
         print("GPT2LMHeadModelMoreh ##################################")
+        if config.moreh_config is not None:
+            print("config.moreh_config")
+            for key, value in config.moreh_config.items():
+                print(f"\t {key}, {value}")
+        else:
+            print("config.moreh_config is None")
         self.transformer = GPT2Model(config)
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
